159f3de16c46eb2d051dd599f48dfc5f0d63a1d6
lightgonmei <lightpuna123@gmail.com>
2025-03-20 11:53:15 +0530

    Added comment

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
-
+#Comment 
 app = Flask(__name__)
 
 # Clean text function
